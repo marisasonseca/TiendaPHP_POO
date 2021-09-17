33439815d3d5e00016dbc1c23688a85816a884a1
ChrisVarg <cristianvargas385@gmail.com>
2021-09-16 19:32:50 -0500

    Finish Index layout     - Layout html and css

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Shirt Shop</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300&display=swap" rel="stylesheet"> 
 </head>
 <body>
     <!-- HEADER -->
     <header class="site-header">
-        <div class="logo">
-            <figure>
+        <div class="header-logo">
+            <figure class="logo">
                 <img src="assets/img/camiseta.png" alt="Camiseta logo" />
             </figure>
             <a href="index.php">
-                SHIRT SHOP
+                shirts shop
             </a>
         </div>
     <!-- Menu -->
@@ -34,6 +39,7 @@
         <!-- SIDERBAR -->
         <aside class="sidebar">
             <div class="block-siderbar sidebar-login">
+                <h3>Login</h3>
                 <form action="#" method="POST">
                     <label for="email">Email</label>
                     <input type="email" name="email">
@@ -43,44 +49,55 @@
 
                     <input type="submit" value="Enter">
                 </form>
-
-                <a href="#">My Orders</a>
-                <a href="#">Orders Management</a>
-                <a href="#">Categories Management</a>
+                <ul>
+                    <li>
+                        <a href="#">My Orders</a>
+                    </li>
+                    <li>
+                        <a href="#">Orders Management</a>
+                    </li>
+                    <li>
+                        <a href="#">Categories Management</a>
+                    </li>
+                </ul>
             </div>
         </aside>
         <div class="main-content">
-            <div class="product">
-                <figure>
-                    <img src="assets/img/camiseta.png" alt="">
-                </figure>
-                <h2>Wide Blue T-Shirt</h2>
-                <p>$30 Euros</p>
-                <a href="#">Buy</a>
-            </div>
-            <div class="product">
-                <figure>
-                    <img src="assets/img/camiseta.png" alt="">
-                </figure>
-                <h2>Wide Blue T-Shirt</h2>
-                <p>$30 Euros</p>
-                <a href="#">Buy</a>
-            </div>
-            <div class="product">
-                <figure>
-                    <img src="assets/img/camiseta.png" alt="">
-                </figure>
-                <h2>Wide Blue T-Shirt</h2>
-                <p>$30 Euros</p>
-                <a href="#">Buy</a>
-            </div>
-            <div class="product">
-                <figure>
-                    <img src="assets/img/camiseta.png" alt="">
-                </figure>
-                <h2>Wide Blue T-Shirt</h2>
-                <p>$30 Euros</p>
-                <a href="#">Buy</a>
+            <h1>Productos Destacados</h1>
+            <div class="content-products">
+                <div class="product">
+                    <figure>
+                        <img src="assets/img/camiseta.png" alt="">
+                    </figure>
+                    <h2>Wide Blue T-Shirt</h2>
+                    <p>$30 Euros</p>
+                    <a class="product-button"  href="#">Buy</a>
+                </div>
+                <div class="product">
+                    <figure>
+                        <img src="assets/img/camiseta.png" alt="">
+                    </figure>
+                    <h2>Wide Blue T-Shirt</h2>
+                    <p>$30 Euros</p>
+                    <a class="product-button" href="#">Buy</a>
+                </div>
+                <div class="product">
+                    <figure>
+                        <img src="assets/img/camiseta.png" alt="">
+                    </figure>
+                    <h2>Wide Blue T-Shirt</h2>
+                    <p>$30 Euros</p>
+                    <a class="product-button" href="#">Buy</a>
+                </div>
+                <div class="product">
+                    <figure>
+                        <img src="assets/img/camiseta.png" alt="">
+                    </figure>
+                    <h2>Wide Blue T-Shirt</h2>
+                    <p>$30 Euros</p>
+                    <a class="product-button" href="#">Buy</a>
+                </div>
+
             </div>
         </div>
     </main>
