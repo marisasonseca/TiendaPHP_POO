d4b3620f95705d7d0bc6482ee8891edf610c80e4
chrisVarg <cristianvargas385@gmail.com>
2021-10-25 15:29:52 -0500

    correction menu login

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,8 +1,14 @@
         <!-- SIDERBAR -->
         <aside class="sidebar">
             <div class="block-siderbar sidebar-login">
+                
+                
                 <?php if(! isset($_SESSION['identity'])) :?>
-                <h3>Login</h3>
+                    <h3>Login</h3>
+                <?php if(isset($_SESSION['errorLogin']) && $_SESSION['errorLogin'] == 'Login Failure') :?>
+                    <h4 class="alert"><?=$_SESSION['errorLogin']?></h4>
+                    <?=Utils::deleteSession('errorLogin')?>
+                <?php endif;?>
                 <form action="<?=BASE_URL?>/user/login" method="POST">
                     <label for="email">Email</label>
                     <input type="email" name="email">
@@ -18,18 +24,25 @@
                 <?php endif;?>
 
                 <ul>
-                    <li>
-                        <a href="#">My Orders</a>
-                    </li>
-                    <li>
-                        <a href="#">Orders Management</a>
-                    </li>
-                    <li>
-                        <a href="#">Categories Management</a>
-                    </li>
-                    <li>
-                        <a href="<?=BASE_URL?>user/logout">Log out</a>
-                    </li>
+                    
+                    <?php if(isset($_SESSION['admin'])) :?>
+
+                        <li>
+                            <a href="#">Orders Management</a>
+                        </li>
+                        <li>
+                            <a href="#">Categories Management</a>
+                        </li>
+                    <?php endif;?>
+                    <?php if(isset($_SESSION['identity'])) :?>
+                        <li>
+                            <a href="#">My Orders</a>
+                        </li>
+                        <li>
+                            <a href="<?=BASE_URL?>user/logout">Log out</a>
+                        </li>
+                    <?php endif;?>
+
                 </ul>
             </div>
         </aside>
